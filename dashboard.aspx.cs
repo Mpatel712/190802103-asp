4937755264724cebdca5ebe32839169679942cc6
Mpatel712 <savsanimegha@gmail.com>
2022-03-05 13:06:46 +0530

    initial commit

---
+++ dashboard.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class dashboard : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // ConnectionString="<%$ ConnectionStrings:DatabaseConnectionString1 %>" 
+          //  DeleteCommand="DELETE FROM [info] WHERE [id] = @id" 
+          //  InsertCommand="INSERT INTO [info] ([name], [email], [password]) VALUES (@name, @email, @password)" 
+           // ProviderName="<%$ ConnectionStrings:DatabaseConnectionString1.ProviderName %>" 
+           // SelectCommand="SELECT [id], [name], [email], [password] FROM [info]" 
+           // UpdateCommand="UPDATE [info] SET [name] = @name, [email] = @email, [password] = @password WHERE [id] = @id">
+        if(Session["email"] == null)
+        {
+            Response.Redirect("~/login.aspx");
+        }
+        else 
+        {
+            Literal1.Text="welcome " + Session["email"].ToString();
+        }
+
+    }
+protected void  Button1_Click(object sender, EventArgs e)
+{
+    Session["email"]=null;
+    Response.Redirect("~/login.aspx");
+}
+}
---
+++ login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class login : System.Web.UI.Page
+{
+    SqlConnection con;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnectionString1"].ConnectionString);
+        // ConnectionString="<%$ ConnectionStrings:DatabaseConnectionString1 %>" 
+        //  DeleteCommand="DELETE FROM [info] WHERE [id] = @id" 
+        //  InsertCommand="INSERT INTO [info] ([name], [email], [password]) VALUES (@name, @email, @password)" 
+        // ProviderName="<%$ ConnectionStrings:DatabaseConnectionString1.ProviderName %>" 
+        // SelectCommand="SELECT [id], [name], [email], [password] FROM [info]" 
+        // UpdateCommand="UPDATE [info] SET [name] = @name, [email] = @email, [password] = @password WHERE [id] = @id">
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        
+               try
+        {
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM [info] WHERE [email] = @email AND [password] = @password", con);
+            cmd.Parameters.AddWithValue("@email", TextBox1.Text.Trim());
+            cmd.Parameters.AddWithValue("@password", TextBox2.Text.Trim());
+            con.Open();
+            int s = (int)cmd.ExecuteScalar();
+            if (s == 1)
+            {
+                Session["email"] = TextBox1.Text;
+                TextBox1.Text = string.Empty;
+                TextBox2.Text = string.Empty;
+                Response.Redirect("~/home.aspx");
+            }
+            else
+            {
+                TextBox1.Text = string.Empty;
+                TextBox2.Text = string.Empty;
+                Literal1.Text = "Email and Password are invalid!";
+            }
+            con.Close();
+        }
+        catch (SqlException ex)
+               { 
+
+            Response.Write("<script>alert('" + ex.ToString() + "')</script>");
+        }
+     }
+}
